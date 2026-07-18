f73e658a96d6ff9b54db3e9e8f11bc407feb0ee6
Mufarhan426 <farhansiddique.mu@gmail.com>
2026-07-18 08:14:40 -0500

    readme file

--- change.sh
+++
@@ -1,9 +0,0 @@
-git filter-branch --env-filter '
-CORRECT_NAME="Mufarhan426"
-CORRECT_EMAIL="farhansiddique.mu@gmail.com"
-
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-' --tag-name-filter cat -- --branches --tags
